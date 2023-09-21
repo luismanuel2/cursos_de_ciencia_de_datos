9bee10186a5b397cfea58b02c2f73613c825e3bd
luismanuel2 <luismanuelambrocio@gmail.com>
2023-09-20 23:55:19 -0600

    Eliminados archivos y carpetas no deseados

--- especializacion_Johns_Hopkins/exploraty data analysis/practice/final_plot1.R
+++
@@ -1,7 +0,0 @@
-NEI <- readRDS("summarySCC_PM25.rds")
-
-
-sub1<-split(NEI,NEI$year)
-x<-c(1999,2002,2005,2008)
-plot(x,sapply(X = sub1,FUN=function(x){sum(x$Emissions)}),main="total emissions",xlab="Year",ylab="ylab",pch=19)
-
